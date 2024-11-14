202a33f335d8eba28b4540361272a7dc0cc23a60
Odra9 <sivori.leonardo09@gmail.com>
2024-11-15 00:38:36 +0100

    prima stesura fatta, necessario refactor in primis e adattare il codice a quanto richiesto dalla dispensa in secundis

--- SkipListPQ.java
+++ SkipListPQ.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class SkipListPQ {
 	private static final int NegInf = Integer.MIN_VALUE, PosInf = Integer.MAX_VALUE;
 	private static final String sentinel = "sentinel";
@@ -5,15 +7,13 @@ public class SkipListPQ {
 	private class Node {
 		Node(int key, String value) {
 			e = new Entry(key, value);
-			next=null; prev=null; above=null; below=null;
+			//next=null; prev=null; above=null; below=null;
 		}
 
 		public int getKey() {return e.getKey();}
 		public String getValue() {return e.getValue();}
 
-		public String print() {
-			return e.print();
-		}
+		public String print() {return e.print();}
 		
 		public Entry e;
 		public Node next, prev, above, below;
@@ -23,6 +23,7 @@ public class SkipListPQ {
 	SkipListPQ(double a) {
 		this.a = a;
 		this.h = 0;
+		this.size = 2;
 
 		//start node
 		s = new Node(NegInf, sentinel);
@@ -31,24 +32,17 @@ public class SkipListPQ {
 	}
 
 	public int size() {
-		int N = 1;
-		Node t = s;
-		while(t.next!=null) {
-			N++;
-			t = t.next;
-		}
-		return N;
+		return size;
 	}
 
 	public Node minNode() {
 		Node minNode = s;
-		Node t = s.next;
-		while(t!=null) {
-			if(t.getKey()<minNode.getKey())
-				minNode=t;
-
-			t=t.next;
+		while(minNode.below!=null) {
+			minNode = minNode.below;
 		}
+
+		if (minNode.next.getValue()!=sentinel) minNode=minNode.next;
+
 		return minNode;
 	}
 
@@ -56,9 +50,85 @@ public class SkipListPQ {
 		return minNode().e;
 	}
 	
-	public void insert(int newKey, int newValue) {
+	public int insert(int newKey, String newValue) {
+		int l = generateEll(0.5f, newKey);
+		//Node ins = new Node(newKey, newValue);
 
+		//push s up
+		while (l>=h) {
+			Node newS = new Node(NegInf, sentinel);
+			s.above = newS;
+			newS.below = s;
+			newS.next = new Node(PosInf, sentinel);
+			newS.next.prev = newS;
+			s.next.above = newS.next;
+			newS.next.below = s.next;
+			s = s.above;
+			h++;
+			size+=2;
+		}
+
+		Node p = s;
+		//push p down
+		for(int i=h-l-1;i>0;i--) {
+			p = p.below;
+		}
+		
+		int steps = 0;
+		Node insAbove = null;
+		do {
+			steps++;
+			p = p.below;
+			while(p.next.getKey() <= newKey) {
+				p = p.next;
+				steps++;
+			}
+
+			Node oldNext = p.next;
+			//insert between p and oldNext
+			p.next = new Node(newKey, newValue);
+			p.next.next = oldNext;
+			oldNext.prev = p.next;
+			p.next.above = insAbove;
+			if(insAbove != null)
+				insAbove.below = p.next;
+			insAbove = p.next;
+			size++;
+		} while(p.below!=null && p.getKey()!=newKey);
+
+		return steps;
 	}
+
+	private int generateEll(float α, int key) {
+		//Random rand = new Random(key);
+		Random rand = new Random();
+		float f = rand.nextFloat();
+		int level = 0;
+
+		while (f<a) {
+			level++;
+			f = rand.nextFloat();
+		}
+
+		return level;
+	}
+
+	//returns (int steps, Node found)
+	/*private Object[] skipSearch(int key) {
+		Node p = s;
+		int steps = 0;
+		do
+			steps++;
+			while(p.next.getKey() < key) {
+				p = p.next;
+				steps++;
+			}
+		} while(p.below!=null && p.getKey()==key);
+
+		if (p.getKey()!=key) p=null;
+		
+		return new Object[]{steps, p};
+	}*/
 
 	public Entry removeMin() {
 		Node min = minNode();
@@ -86,9 +156,16 @@ public class SkipListPQ {
 
 	public String print() {
 		Node t = s;
+		while(t.below!=null){t=t.below;}
 		String str = "";
 		do {
-			str += t.print() + "\n";
+			int countLevel = 1;
+			Node p = t;
+			while(p.above!=null) {
+				p = p.above;
+				countLevel++;
+			}
+			str += t.print() + " " + countLevel + "\n";
 			t = t.next;
 		}	while(t!=null);
 
@@ -96,6 +173,6 @@ public class SkipListPQ {
 	}
 
 	private double a;
-	private int h;
+	private int h, size;
 	private Node s;
 }
--- TestProgram.java
+++ TestProgram.java
@@ -2,6 +2,17 @@ public class TestProgram {
 	public static void main(String[] args) {
 		SkipListPQ s = new SkipListPQ(0.5);
 		
-		System.out.println("Struttura:\n " + s.print() + "\n\nDimensione: " + s.size() + "\nMin: " + s.min().print());
+		//System.out.println("Struttura:\n " + s.print() + "\n\nDimensione: " + s.size() + "\nMin: " + s.min().print() + "\n");
+
+		s.insert(0, "a");
+		s.insert(1, "b");
+
+		//System.out.println("Struttura:\n " + s.print() + "\n\nDimensione: " + s.size() + "\n");
+
+		s.insert(3, "c");
+		s.insert(2, "d");
+		s.insert(0, "e");
+
+		System.out.println("Struttura:\n " + s.print() + "\n\nDimensione: " + s.size() + "\n");
 	}
 }
